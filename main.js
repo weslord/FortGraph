8c17fd1c3fb2b9723a6d7126dde53699b0befaeb
weslord <weslord@gmail.com>
2018-05-16 13:43:52 -0400

    Change object edit dialogue to inspector

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 { // INIT
-  var width  = 960,
+  var width  = 800,
       height = 500;
+
+  var color = d3.scaleOrdinal(d3.schemeCategory10);
 
   var svg = d3.select('body').append('svg')
       .attr('width', width)
@@ -11,6 +13,21 @@
 
   var linePreview = svg
      .append('line');
+
+  var inspector = d3.select('body').append('div')
+      .attr('id', 'inspector');
+  var titleInput = inspector.append('input')
+      .on('change', function () {
+        selected ? selected.id = this.value : null;
+        update();
+      })
+      .node();
+  var typeInput = inspector.append('input')
+      .on('change', function () {
+        selected ? selected.type = this.value : null
+        update();
+      })
+      .node();
 
   var lastVertexId = 2;
   var vertices = [
@@ -25,6 +42,7 @@
 
   var source = null,
       target = null,
+      selected = null,
       line   = null; // rename this (represents an edge)
 
   var dragging = true;
@@ -43,8 +61,8 @@
       .force('charge', d3.forceManyBody().strength(-200))
       .on('tick', tick);
 
-  simulation.force('x').strength(0.05);
-  simulation.force('y').strength(0.10);
+  simulation.force('x').strength(0.04);
+  simulation.force('y').strength(0.08);
 
   update();
 }
@@ -84,30 +102,18 @@ function update() {
          return 'translate(-'+ b.width/2 +','+ b.height/2 +')';
         });
   buildings.selectAll('rect')
+      .attr('stroke', function (d) {return color(d.type);})
       .attr('rx', 5)
       .attr('ry', 5)
       .attr('width', 50)
       .attr('height', 50)
       .attr('transform', 'translate(-25,-25)');
 
-/*
-      .attr('width', function(d) {
-        return this.previousElementSibling.getBBox().width + 5;
-      })
-      .attr('height', function(d) {
-        return this.previousElementSibling.getBBox().height + 5;
-      })
-      .attr('transform', function() {
-       var b = this.previousElementSibling.getBBox();
-       return 'translate(-'+ b.width/2 +',-'+ b.height/2 +')';
-      });
-*/
-
   d3.selectAll('line').lower();
   d3.selectAll('text').raise();
 
   simulation.nodes(vertices);
-  simulation.force('link').links(edges).distance(150).strength(0.1)
+  simulation.force('link').links(edges).distance(100).strength(0.2)
   simulation.alpha(0.3).restart();
 
 }
@@ -124,12 +130,14 @@ function tick() {
   });
 }
 
-// if control-clicking, should start a control-drag event...
 function newVertexAtMouse() {
   var x = d3.mouse(this)[0];
   var y = d3.mouse(this)[1];
 
-  vertices.push({id: ++lastVertexId, x: x, y: y});
+  var newVertex = {id: 'New #'+ ++lastVertexId, x: x, y: y};
+
+  vertices.push(newVertex);
+  selectObj(newVertex);
 
   update();
 }
@@ -218,11 +226,23 @@ function lineUnHover(d) {
 }
 
 function selectObj(subject) {
-  if (subject.selected) {
-    subject.selected = false;
-  } else {
+    selected = subject;
+
+    edges.forEach(function(edge) {
+      edge.selected = false;
+    });
+
+    vertices.forEach(function(vertex) {
+      vertex.selected = false;
+    });
+
+  if (subject) {
     subject.selected = true;
+
+    titleInput.value = subject.id;
+    typeInput.value = subject.type;
   }
+
   update();
 }
 
@@ -234,37 +254,29 @@ function edgeExists(source, target) {
     }
   }
 }
-var x;
-function editLabel(d) {
-  if (d) {
-    var label = prompt('Building type:')
-    if (label) {
-      d.id = label;
-      update();
-    }
-  }
-}
 
-function windowKeydown() {
-  switch(d3.event.keyCode) {
-    case 16: // shift
-    case 17: // ctrl
-    case 18: // alt
-    case 91: // cmd
-      ctrlPressed = true;
-      svg.attr('style', 'cursor: pointer');
-      break;
-    case 8:  // backspace
-    case 46: // delete
-    case 68: // d
-      svg.selectAll('.selected').each(deleteObj);
-      target = null;
-      break;
-    case 69: // e
-      editLabel(target);
-      break;
-    default:
-      break;
+function windowKeydown(d) {
+  if (d3.event.target.type !== "text") {
+    switch(d3.event.keyCode) {
+      case 16: // shift
+      case 17: // ctrl
+      case 18: // alt
+      case 91: // cmd
+        ctrlPressed = true;
+        svg.attr('style', 'cursor: pointer');
+        break;
+      case 8:  // backspace
+      case 46: // delete
+      case 68: // d
+        svg.selectAll('.selected').each(deleteObj);
+        target = null;
+        break;
+      case 27: // esc
+        selectObj(null);
+        break;
+      default:
+        break;
+    }
   }
 }
 
