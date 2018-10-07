2f8c0bec0f0b44e6420bb8d7a8c64bb549686167
weslord <weslord@gmail.com>
2018-10-07 01:49:09 -0700

    Improve appearance of arrows

--- main.js
+++ main.js
@@ -27,14 +27,14 @@
   // define arrow markers for graph links
   svg.append('svg:defs').append('svg:marker')
     .attr('id', 'end-arrow')
-    .attr('viewBox', '0 -5 10 10')
-    .attr('refX', 6)
-    .attr('markerWidth', 3)
-    .attr('markerHeight', 3)
+    .attr('viewBox', '0 -5 15 10')
+    .attr('refX', 7.5)
+    .attr('markerWidth', 7.5)
+    .attr('markerHeight', 5)
     .attr('orient', 'auto')
   .append('svg:path')
-    .attr('d', 'M0,-5L10,0L0,5')
-    .attr('style', 'fill: #000');
+    .attr('d', 'M 0 -5 L 10 0 L 0 5')
+    .attr('style', 'fill: #000; stroke: none');
 
   var buildings = world.selectAll('g');
   var lines     = world.selectAll('g');
